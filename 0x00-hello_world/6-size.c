dacaf31025cd9b45f934e946eb5a18b38a953c9d
OmarrAhmedd <omarrahmedredaa@gmail.com>
2022-09-08 19:13:16 -0700

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - print the strin in the put function
- * prints the size of various types on the computer it is compiled and run on
+ *
+ * prints the size of various types on the computer
  * Return: ALways 0
  */
+
 int main(void)
 {
-	char c;
-	int f;
-	long int il;
-	long long int ilil;
-	float v;
- 	
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(f));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(il));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ilil));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v));
-	return(0);
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
