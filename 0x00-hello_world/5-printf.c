478af426bb6021999cebd77c2f876d6d1f584ae2
OmarrAhmedd <omarrahmedredaa@gmail.com>
2022-09-08 18:04:47 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - print the string in printf function
  * Descrption : Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,
+ * prints exactly with proper grammar, but the outcome is a piece of art,
  * Return: Always 0 (Success)
  */
-
 int (main)void
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
