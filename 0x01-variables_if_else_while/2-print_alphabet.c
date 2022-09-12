a7293c6a9d045a78e06146bd35038614ce4fba16
OmarrAhmedd <omarrahmedredaa@gmail.com>
2022-09-11 19:03:19 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ * main - withe alpahbet in lowercase
+ *
+ * Descreption - "print lowercase alphabet"
+ *
+ * Return : 0
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch => 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
