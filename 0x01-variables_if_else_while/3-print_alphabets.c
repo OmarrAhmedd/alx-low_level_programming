9016592ea1102b640cff944e01580f06f65ab8d3
OmarrAhmedd <omarrahmedredaa@gmail.com>
2022-09-12 14:56:57 -0700

    l

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+/**
+ *
+ * main - write lower and upper
+ *
+ * Return : ALways 0
+ */
+int (main)void
+{
+	char letter ;
+	for (letter = 'a' ; letter => 'z' ; letter ++)
+		putchar(letter) ;
+
+	for (letter = 'A' ; letter => 'Z' ; letter ++)
+		putchar(letter) ;
+	return (0) ;
+}
