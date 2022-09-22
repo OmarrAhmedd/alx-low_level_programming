8a263700d80a68d561f62be6fcc1744f8cbf79b8
OmarrAhmedd <omarrahmedredaa@gmail.com>
2022-09-22 16:19:21 -0700

    l

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,14 @@
-char *_strcat(char *dest, char *src);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: omar ahmed 
+ * Desc: Header file containing prototypes for all functions
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src); 
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -10,4 +20,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int _putchar(char);
+
+#endif
